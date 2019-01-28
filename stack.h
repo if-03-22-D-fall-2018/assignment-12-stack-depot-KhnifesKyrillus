ccee6c025385d848f566cd89a15c8122f34be729
Kyrillus Khnifes <43353117+KhnifesKyrillus@users.noreply.github.com>
2019-01-28 22:33:03 +0100

    Add files via upload
    finished progam

---
+++ stack.h
@@ -0,0 +1,15 @@
+#include "memcheck.h"
+
+#ifndef SPL_STACK_H
+#define SPL_STACK_H
+
+typedef struct StackImplementation* Stack;
+
+Stack create_stack();
+void delete_stack(Stack stack);
+void push_stack(Stack stack, void *data);
+int get_count(Stack stack);
+void* pop_stack(Stack stack);
+void* peek_stack(Stack stack);
+
+#endif
